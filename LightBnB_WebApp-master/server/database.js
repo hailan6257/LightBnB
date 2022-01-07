58474d8abd423d53d22befa94256d66ca0c711a8
Hailan Wang <hailan6257@email.com>
2022-01-07 02:54:17 -0500

    Add sql to add new property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -95,8 +95,8 @@ const getAllProperties = (options, limit = 10) => {
   }
   //4
   if (options.owner_id) {
-    queryParams.push(`%${options.owner_id}%`);//like '%%' = no where clause
-    queryString += `AND owner_id = $${queryParams.indexOf(`%${options.owner_id}%`) + 1} `;
+    queryParams.push(options.owner_id);//like '%%' = no where clause
+    queryString += `AND owner_id = $${queryParams.indexOf(options.owner_id) + 1} `;
   }
   //5
   if (options.minimum_price_per_night) {
@@ -123,10 +123,15 @@ const getAllProperties = (options, limit = 10) => {
   `;
 
   // 9
-  // console.log(queryString, queryParams);
+  console.log(queryString, queryParams);
 
   // 10
-  return pool.query(queryString, queryParams).then((res) => res.rows);
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => {
+      console.log(res.rows);
+      return res.rows;
+    });
 };
 
 
@@ -138,10 +143,27 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = (property) => {
+  const text = `INSERT INTO properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms) 
+  VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *`;
+  const values = [property.owner_id,property.title,property.description,property.thumbnail_photo_url,property.cover_photo_url,property.cost_per_night,property.street,property.city,property.province,property.post_code,property.country,property.parking_spaces,property.number_of_bathrooms,property.number_of_bedrooms];
+  return pool
+    .query(text, values)
+    .then((result) => result.rows[0])//test with password:wang, w@w.com, password
+    .catch((err) => console.log(err.message));
 };
 exports.addProperty = addProperty;
